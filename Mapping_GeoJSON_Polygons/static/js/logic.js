d7b706055593e6622790ec88f62af9893007f5c7
unknown <hannahc1@gmail.com>
2020-06-26 00:35:29 -0500

    Module 13.5.6

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,51 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map. (see module 13.2.4 to change)
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id:'streets-v11',
+	accessToken: API_KEY
+});//.addTo(map);
+// Add a night time layer.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id:'satellite-streets-v11',
+	accessToken: API_KEY
+});//.addTo(map);
+// Create the map object with center and zoom level.
+let map = L.map('mapid',{
+	center: [43.7, -79.3],
+	zoom: 11,
+	layers: [satelliteStreets]
+  });
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/hannahc1/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+// Grabbing our GeoJSON data.
+// Create a style for the lines.
+let myStyle = {
+	color: "blue",
+	fillColor: "yellow",
+	weight: 1
+}
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+   
+  L.geoJson(data, {
+	style: myStyle,
+	onEachFeature: function(feature, layer) {
+    		console.log(layer);
+    		layer.bindPopup("<h3>" + feature.properties.AREA_NAME + "</h3>");
+    	  }})
+  .addTo(map);
+});
+var baseMaps = {
+	"Streets": streets,
+	"Satellite Streets": satelliteStreets
+};
+
+
+// Then we add our 'streets' tile layer to the map.
+L.control.layers(baseMaps).addTo(map);
